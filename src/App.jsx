f741cd6fb5ed5e6fbece5a7916451fac095176b7
shahin2525 <shahin451999@gmail.com>
2024-06-20 11:12:10 -0700

    practice props

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+/* eslint-disable react/prop-types */
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
       <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Student name="rakib" age="20"></Student>
+      <Student name="sakib" age="10"></Student>
+      <Student></Student>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
+
+function Student({ name = "m", age = "33" }) {
+  const style = {
+    margin: "20px",
+    padding: "20px",
+    border: "2px solid blue",
+    borderRadius: "4px",
+  };
+  return (
+    <div style={style}>
+      <p>name:{name}</p>
+      <p>age:{age}</p>
+    </div>
+  );
+}
