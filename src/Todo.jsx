f5bdc00f68293a5db8826b7012ce4b80cace73bb
shahin2525 <shahin451999@gmail.com>
2024-06-21 18:47:54 -0700

    practice more conditional rendaring

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -1,15 +1,34 @@
 /* eslint-disable react/prop-types */
-const Todo = ({ task, isDone }) => {
-  if (isDone) {
-    return <li>Finished: {task}</li>;
-  }
-  return <li>task on : {task}</li>;
-};
+// const Todo = ({ task, isDone }) => {
+//   if (isDone) {
+//     return <li>Finished: {task}</li>;
+//   }
+//   return <li>task on : {task}</li>;
+// };
 
-export default Todo;
+// export default Todo;
 // export default function Todo({ task, isDone }) {
 //   if (isDone) {
 //     return <li>Task: {task}</li>;
 //   }
 //   return <li>task on : {task}</li>;
 // }
+
+// const Todo = ({ task, isDone }) => {
+//   return (
+//     <li>
+//       {isDone || "do it"}: {task}
+//     </li>
+//   );
+// };
+
+const Todo = ({ task, isDone }) => {
+  let listItem;
+  if (isDone) {
+    listItem = <li>Finished: {task}</li>;
+  } else {
+    listItem = <li>work on : {task}</li>;
+  }
+  return listItem;
+};
+export default Todo;
